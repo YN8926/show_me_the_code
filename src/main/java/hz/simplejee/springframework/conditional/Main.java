55ab8f44dfda07b336d4fcfeb2c258ebcdc3e109
辉辉大侠 <xredman@yeah.net>
2016-10-30 21:09:04 +0800

    spring job和condition测试

---
+++ src/main/java/hz/simplejee/springframework/conditional/Main.java
@@ -0,0 +1,7 @@
+package hz.simplejee.springframework.conditional;
+
+/**
+ * Created by 辉辉大侠 on 30/10/2016.
+ */
+public class Main {
+}
---
+++ src/main/java/hz/simplejee/springframework/conditional/OSXCondition.java
@@ -0,0 +1,15 @@
+package hz.simplejee.springframework.conditional;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * Created by 辉辉大侠 on 30/10/2016.
+ */
+public class MacCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        return context.getEnvironment().getProperty("os.name").contains("Mac OS X");
+    }
+}
